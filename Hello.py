5bd49180c49296bcc9ee0cf2b679b80f695eeeab
danikagupta <74515001+danikagupta@users.noreply.github.com>
2024-01-09 00:33:21 +0000

    Initial commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,38 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import time
 
-LOGGER = get_logger(__name__)
+st.title("UI for waiting!!")
+
+msg_id=0
+
+def show_message(start_time,end_time,t,msgs):
+  global msg_id
+  time_taken=round(time.time()-start_time)
+  if(time.time()>end_time):
+    msg=f"## Processing took {time_taken} seconds"
+    return msg
+  msg=msgs[time_taken//t % len(msgs)]
+  ts=time.strftime("%H:%M:%S", time.localtime())
+  tf=f"0:{time_taken}"
+  if(time_taken<10):
+    tf=f"0:0{time_taken}"
+  if(time_taken>=60):
+    mins=time_taken//60
+    secs=time_taken-mins*60
+    tf=f"{mins}:{secs}"
+  return f"## {ts} \n## {tf}\n## {msg}"
+
+def runFor(tot,t,msgs):
+  start_time = time.time()
+  end_time=time.time()+tot
+
+  while True:
+    print(f"In the loop at {time.time()} with start at {start_time} and end at {end_time}")
+    with st.spinner(show_message(start_time,end_time,t,msgs)):
+      time.sleep(t)
+    if(time.time()>end_time):
+      st.write(show_message(start_time,end_time,t,msgs))
+      break;
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+runFor(15,1,["ABC","DEF","GHI","JKL"])
